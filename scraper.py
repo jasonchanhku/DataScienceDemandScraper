56824e4360bd96fb9d26b9997768810451a751f1
Jason Chan <jasonchanhku@gmail.com>
2018-04-09 16:23:33 +0800

    disabled shm usage

--- scraper.py
+++ scraper.py
@@ -11,7 +11,8 @@ import sqlite3
 
 # Headless option
 options2 = webdriver.ChromeOptions()
-options2.add_argument('headless')
+options2.add_argument('--headless')
+options2.add_argument('--disable-dev-shm-usage')
 
 # get web driver up and running
 driver = webdriver.Chrome(chrome_options=options2)
